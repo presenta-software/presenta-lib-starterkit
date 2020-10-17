0f057257e668608c1d23c9ee0183dfaf07e68826
abusedmedia <ff@todo.to.it>
2020-10-17 11:21:43 +0200

    lib updated

--- config.js
+++ config.js
@@ -1,44 +1,42 @@
 const config = {
-  transition: '.verticalIn',
-  modules: {
-    markdown: true
+  colors: 'a',
+  transition: 'a',
+  blockVar: 'c',
+  controllers: {
+    fullscreen: true,
+    focus: true
   },
   scenes: [
-
     {
       blocks: [
         {
-          type: 'text',
-          text: '# Slide 1',
           scale: 3,
-          props: {
-            colorvar: '.b'
-          }
+          type: 'text',
+          text: '<h1>Hello <b>PRESENTA!</b></h1>'
         }
-      ]
+      ],
+      colorVar: 'b'
     },
-
     {
       blocks: [
         {
+          scale: 2,
           type: 'text',
-          text: '# Slide 2',
-          scale: 3,
-          props: {
-            colorvar: '.c',
-            textPosition: '.tl',
-            textAlign: 'left'
-          }
+          text: '<h2>Scene 2</h2>'
         }
-      ]
+      ],
+      colorVar: 'a'
     },
-
     {
       blocks: [
         {
+          scale: 2,
           type: 'text',
-          text: '# Slide 1',
-          scale: 3
+          text: '<h2>Scene 3</h2>'
+        },
+        {
+          type: 'image',
+          url: 'https://images.unsplash.com/photo-1562797807-aa9baed9a414?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
         }
       ]
     }
