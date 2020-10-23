0031826220da84fbd71a038eb3b59dbc1402a8cc
abusedmedia <ff@todo.to.it>
2020-10-23 18:05:31 +0200

    updated

--- config.js
+++ config.js
@@ -1,42 +1,73 @@
 const config = {
-  colors: 'a',
-  transition: 'a',
+  colors: 'adele',
+  transition: 'fadeIn',
   blockVar: 'c',
   controllers: {
     fullscreen: true,
-    focus: true
+    focus: true,
+    swiper: true,
+    markdown: true
+  },
+  modules: {
+    highlightjs: 'monokai'
   },
   scenes: [
     {
       blocks: [
         {
-          scale: 3,
+          textVar: 'title',
           type: 'text',
           text: '<h1>Hello <b>PRESENTA!</b></h1>'
         }
       ],
-      colorVar: 'b'
+      colorVar: 'sec'
     },
     {
       blocks: [
         {
-          scale: 2,
+          textVar: 'title',
           type: 'text',
           text: '<h2>Scene 2</h2>'
         }
       ],
-      colorVar: 'a'
+      colorVar: 'main'
     },
     {
       blocks: [
         {
-          scale: 2,
+          textVar: 'title',
           type: 'text',
           text: '<h2>Scene 3</h2>'
         },
         {
           type: 'image',
           url: 'https://images.unsplash.com/photo-1562797807-aa9baed9a414?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+        }
+      ]
+    },
+    {
+      blocks: [
+        {
+          type: 'text',
+          text: `## This block:
+\`\`\`js
+{
+  type: 'text',
+  text: '## This block:'
+}
+\`\`\`
+`,
+          textVar: 'title'
+        }
+      ],
+      colorVar: 'main'
+    },
+    {
+      blocks: [
+        {
+          blockVar: '',
+          type: 'youtube',
+          url: 'Mh4f9AYRCZY'
         }
       ]
     }
